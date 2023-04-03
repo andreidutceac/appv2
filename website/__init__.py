30aebd86a372d76c27746ef2ead9304dcea8e57b
andreidutceac <117718437+andreidutceac@users.noreply.github.com>
2023-04-03 22:23:14 +0300

    update home

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sdfsdfsfvrvf'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://coqbzbhibswkan:5a7468686a129bb417b631d8591d7f6e1874b12fe07abf72fc0bc1651a4d6a0a@ec2-99-80-190-165.eu-west-1.compute.amazonaws.com:5432/dfona0oua7dv66"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://coqbzbhibswkan:5a7468686a129bb417b631d8591d7f6e1874b12fe07abf72fc0bc1651a4d6a0a@ec2-99-80-190-165.eu-west-1.compute.amazonaws.com:5432/dfona0oua7dv66"
     db.init_app(app)
 
 
