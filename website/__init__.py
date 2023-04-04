472ba57cd023f00f27bd3d05341d448d952b363a
andreidutceac <117718437+andreidutceac@users.noreply.github.com>
2023-04-04 10:59:21 +0300

    postgresql

--- website/__init__.py
+++ website/__init__.py
@@ -12,10 +12,11 @@ def create_app():
     app = Flask(__name__)
     #DATABASE_URL = os.environ['DATABASE_URL']
     #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "postgres://fqznwmzmqiceex:738c7ea048798ed49afaa61d99a91273ee3eb42ff704ddaa2ed4c9a5c8dc5d99@ec2-34-251-233-253.eu-west-1.compute.amazonaws.com:5432/d1i3qosk54evtj"
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", f'sqlite:///{DB_NAME}')
+    # app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+    DATABASE_URL1 = "postgresql://fqznwmzmqiceex:738c7ea048798ed49afaa61d99a91273ee3eb42ff704ddaa2ed4c9a5c8dc5d99@ec2-34-251-233-253.eu-west-1.compute.amazonaws.com:5432/d1i3qosk54evtj"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL1
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", f'sqlite:///{DB_NAME}')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://coqbzbhibswkan:5a7468686a129bb417b631d8591d7f6e1874b12fe07abf72fc0bc1651a4d6a0a@ec2-99-80-190-165.eu-west-1.compute.amazonaws.com:5432/dfona0oua7dv66"
     db.init_app(app)
